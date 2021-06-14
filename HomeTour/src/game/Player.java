67437a4da1a02d403d33d5f41b7547fa31fa65e0
Mohammad Reza Amjad <mohammadrezaamjad@Mohammads-MacBook-Pro.local>
2021-06-13 17:58:00 -0700

    files saved

--- HomeTour/src/game/Player.java
+++ HomeTour/src/game/Player.java
@@ -1,5 +1,30 @@
 package game;
+import java.util.*;
+import fixtures.Room;
 
 public class Player {
-
+	Stack<Room> visitedRoomList;
+	
+	private String name;
+	
+	public Player() {
+		visitedRoomList = new Stack<Room>();
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void addRoomToList(Room current) {
+		visitedRoomList.add(current);
+	}
+	
+	public String getCurrentRoom() {
+		Room curr = visitedRoomList.peek();
+		return curr.getName();	
+	}
 }
